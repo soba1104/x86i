1843c507d7e16cc202e9b28b25d4a609d819582e
soba1104 <shiba@rvm.jp>
2015-09-20 17:07:02 +0900

    instr.h に差がなくなったので著作権表示もオリジナルのものに戻す

--- src/bochs/instr.h
+++ src/bochs/instr.h
@@ -1,33 +1,25 @@
-/////////////////////////////////////////////////////////////////////////////////
-// Copyright (C) 2015 satoshi shiba
-// Copyright (C) 2008-2012 Stanislav Shwartsman
+/////////////////////////////////////////////////////////////////////////
+// $Id: instr.h 12767 2015-05-16 20:29:49Z sshwarts $
+/////////////////////////////////////////////////////////////////////////
 //
-// Original source of this file is 'cpu/instr.h'.
-// You can download original source from following link.
-// http://sourceforge.net/projects/bochs/files/bochs/2.6.8/
-//  -------------------------- Original Copyright ------------------------------
-// |////////////////////////////////////////////////////////////////////////////|
-// |// $Id: instr.h 12767 2015-05-16 20:29:49Z sshwarts $                       |
-// |////////////////////////////////////////////////////////////////////////////|
-// |                                                                            |
-// | Copyright (c) 2008-2012 Stanislav Shwartsman                               |
-// |        Written by Stanislav Shwartsman [sshwarts at sourceforge net]       |
-// |                                                                            |
-// | This library is free software; you can redistribute it and/or              |
-// | modify it under the terms of the GNU Lesser General Public                 |
-// | License as published by the Free Software Foundation; either               |
-// | version 2 of the License, or (at your option) any later version.           |
-// |                                                                            |
-// | This library is distributed in the hope that it will be useful,            |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of             |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU          |
-// | Lesser General Public License for more details.                            |
-// |                                                                            |
-// | You should have received a copy of the GNU Lesser General Public           |
-// | License along with this library; if not, write to the Free Software        |
-// | Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA B 02110-1301 USA |
-//  ----------------------------------------------------------------------------
-/////////////////////////////////////////////////////////////////////////////////
+//   Copyright (c) 2008-2012 Stanislav Shwartsman
+//          Written by Stanislav Shwartsman [sshwarts at sourceforge net]
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA B 02110-1301 USA
+//
+/////////////////////////////////////////////////////////////////////////
 
 #ifndef BX_INSTR_H
 #define BX_INSTR_H
