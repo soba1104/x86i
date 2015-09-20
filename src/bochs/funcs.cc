337109fb3ebcc28ebb20030d61b38ba68833ca8e
soba1104 <shiba@rvm.jp>
2015-09-21 03:32:08 +0900

    対応する命令を増やす

--- src/bochs/funcs.cc
+++ src/bochs/funcs.cc
@@ -69,6 +69,21 @@ BX_INSF_TYPE BX_CPP_AttrRegparmN(1) BX_CPU_C::CALL_Jq(bxInstruction_c *i)
   RSP -= 8;
 }
 
+// ctrl_xfer64.cc
+BX_INSF_TYPE BX_CPP_AttrRegparmN(1) BX_CPU_C::JMP_EqR(bxInstruction_c *i)
+{
+  Bit64u op1_64 = BX_READ_64BIT_REG(i->dst());
+
+#if 0
+  if (! IsCanonical(op1_64)) {
+    BX_ERROR(("%s: canonical RIP violation", i->getIaOpcodeNameShort()));
+    exception(BX_GP_EXCEPTION, 0);
+  }
+#endif
+
+  RIP = op1_64;
+}
+
 // arith64.cc
 BX_INSF_TYPE BX_CPP_AttrRegparmN(1) BX_CPU_C::SUB_EqIdR(bxInstruction_c *i)
 {
@@ -80,4 +95,12 @@ BX_INSF_TYPE BX_CPP_AttrRegparmN(1) BX_CPU_C::SUB_EqIdR(bxInstruction_c *i)
   BX_WRITE_64BIT_REG(i->dst(), diff_64);
 
   SET_FLAGS_OSZAPC_SUB_64(op1_64, op2_64, diff_64);
+}
+
+// load.cc
+BX_INSF_TYPE BX_CPP_AttrRegparmN(1) BX_CPU_C::LOAD_Eq(bxInstruction_c *i)
+{
+  bx_address eaddr = BX_CPU_RESOLVE_ADDR_64(i);
+  ReadHostQWordFromLittleEndian((Bit64u*)eaddr, TMP64);
+  BX_CPU_CALL_METHOD(i->execute2(), (i));
 }
