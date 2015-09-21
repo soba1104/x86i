9fbe606c510a879d2eefeae7a6b3ed4a87d37477
soba1104 <shiba@rvm.jp>
2015-09-21 19:50:37 +0900

    対応する命令を増やす

--- src/bochs/funcs.cc
+++ src/bochs/funcs.cc
@@ -237,6 +237,14 @@ BX_INSF_TYPE BX_CPP_AttrRegparmN(1) BX_CPU_C::JZ_Jq(bxInstruction_c *i)
 }
 
 // ctrl_xfer64.cc
+BX_INSF_TYPE BX_CPP_AttrRegparmN(1) BX_CPU_C::JNB_Jq(bxInstruction_c *i)
+{
+  if (! get_CF()) {
+    branch_near64(i);
+  }
+}
+
+// ctrl_xfer64.cc
 BX_INSF_TYPE BX_CPP_AttrRegparmN(1) BX_CPU_C::JNZ_Jq(bxInstruction_c *i)
 {
   if (! get_ZF()) {
